60abedb689970bcfd6a4899dd6c30551c502cb68
aizhangyao <819647113@qq.com>
2019-10-15 22:47:34 +0800

    feat:add ImpDemo.java

---
+++ src/com/aiz/se/day02/ImpDemo.java
@@ -0,0 +1,21 @@
+package com.aiz.se.day02;
+
+import java.util.Date;
+
+/**
+ * @ClassName ImpDemo
+ * @Description TODO
+ * @Author ZhangYao
+ * @Date Create in 19:07 2019/10/14
+ * @Version 1.0
+ */
+public class ImpDemo {
+    /**
+     * 主方法
+     * @param args
+     */
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("当前日期为:"+now);
+    }
+}
---
+++ src/com/aiz/se/day02/VarDemo.java
@@ -0,0 +1,11 @@
+package com.aiz.se.day02;
+
+/**
+ * @ClassName VarDemo
+ * @Description TODO
+ * @Author ZhangYao
+ * @Date Create in 19:07 2019/10/14
+ * @Version 1.0
+ */
+public class VarDemo {
+}
