2e49c5604b643b5d728edc9b943a2323a1d229b3
aizhangyao <819647113@qq.com>
2019-10-15 22:48:57 +0800

    feat:add VarDemo.java

--- src/com/aiz/se/day02/VarDemo.java
+++ src/com/aiz/se/day02/VarDemo.java
@@ -8,4 +8,32 @@ package com.aiz.se.day02;
  * @Version 1.0
  */
 public class VarDemo {
+    /**
+     * 主方法
+     * @param args
+     */
+    public static void main(String[] args) {
+        //定义变量
+        int i= 19;
+        long a = 100L;
+        float weight = 67.5F;
+        boolean b = true;
+        char c = 'A';
+        //定义字符串对象变量
+        String website = "http://www.aizhangyao.com";
+        //打印输出以上变量
+        System.out.println("变量i的值:"+i);
+        System.out.println("变量a的值:"+a);
+        System.out.println("变量weight的值:"+weight);
+        System.out.println("变量b的值:"+b);
+        System.out.println("变量c的值:"+c);
+        System.out.println("变量website的值:"+website);
+        //注：任何数据类型与字符串做+运算，其结果都是字符串的拼接。
+        System.out.println("--------------------------------");
+        i = 200;
+        b = false;
+        System.out.println("变量i的值:"+i);
+        System.out.println("变量b的值:"+b);
+    }
+
 }
