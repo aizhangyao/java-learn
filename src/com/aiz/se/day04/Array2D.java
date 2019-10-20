1c8d512c2d12a0e2b948ba48991bbc1af2b7c007
aizhangyao <819647113@qq.com>
2019-10-20 22:59:36 +0800

    feat:add example for array

---
+++ src/com/aiz/se/day04/Array2D.java
@@ -0,0 +1,11 @@
+package com.aiz.se.day04;
+
+/**
+ * @ClassName Array2D
+ * @Description TODO
+ * @Author ZhangYao
+ * @Date Create in 17:21 2019/10/17
+ * @Version 1.0
+ */
+public class Array2D {
+}
---
+++ src/com/aiz/se/day04/ArrayOperate.java
@@ -0,0 +1,11 @@
+package com.aiz.se.day04;
+
+/**
+ * @ClassName ArrayOperate
+ * @Description TODO
+ * @Author ZhangYao
+ * @Date Create in 17:20 2019/10/17
+ * @Version 1.0
+ */
+public class ArrayOperate {
+}
