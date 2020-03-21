169e7891422909b9adc2f51d0d6d0bff1343ab27
aizhangyao <819647113@qq.com>
2020-03-21 23:40:54 +0800

    add strategy design pattern

---
+++ src/com/aiz/dp/strategy/Main.java
@@ -0,0 +1,11 @@
+package com.aiz.dp.strategy;
+
+/**
+ * @ClassName: Main
+ * @Description: TODO
+ * @Author: ZhangYao
+ * @Date: Create in 23:39 2020/3/21
+ * @Version 1.0
+ */
+public class Main {
+}
