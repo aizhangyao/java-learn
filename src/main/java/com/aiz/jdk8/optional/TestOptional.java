84c273e239d97e9600ac8f2105ba5ce8ef930328
zhangyao <zhangyao_xz@qq.com>
2023-04-26 17:11:28 +0800

    add Java8新特性 Optional

---
+++ src/main/java/com/aiz/jdk8/optional/TestOptional.java
@@ -0,0 +1,26 @@
+package com.aiz.jdk8.optional;
+
+import java.util.Optional;
+
+/**
+ * @author ZhangYao
+ * @version 1.0
+ * @className TestOptional
+ * @description TestOptional
+ * @date Create in 16:53 2023/4/26
+ */
+public class TestOptional {
+    public static void main(String[] args) {
+        Optional<String> fullName = Optional.ofNullable(null);
+        System.out.println("Full Name is set? " + fullName.isPresent());
+        System.out.println("Full Name: " + fullName.orElseGet(() -> "[none]"));
+        System.out.println(fullName.map(s -> "Hey " + s + "!").orElse("Hey Stranger!"));
+
+
+        Optional<String> firstName = Optional.of("Tom");
+        System.out.println("First Name is set? " + firstName.isPresent());
+        System.out.println("First Name: " + firstName.orElseGet(() -> "[none]"));
+        System.out.println(firstName.map(s -> "Hey " + s + "!").orElse("Hey Stranger!"));
+        System.out.println();
+    }
+}
